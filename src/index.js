f0716a075cae8570a2e88d4fc51c46fce6ee48f0
cooms <thcoomer@hotmail.com>
2016-08-09 17:09:35 +1200

    added event listeners

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 var wombleList = require('./templates/womble-list.hbs')
+var headerTemplate = require('./templates/header.hbs')
+var footerTemplate = require('./templates/footer.hbs')
+var links = require('./templates/sidebar.hbs')
+
+document.addEventListener('DOMContentLoaded', function () {
+  var div = document.createElement('div')
+  div.innerHTML = headerTemplate()
+  document.body.appendChild(div)
+})
 
 document.addEventListener('DOMContentLoaded', function () {
   var div = document.createElement('div')
@@ -10,5 +19,24 @@ document.addEventListener('DOMContentLoaded', function () {
     ]
   }
   div.innerHTML = wombleList(data)
+  document.body.appendChild(div)
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+  var div = document.createElement('div')
+  var data = {
+    links: [
+      {name: 'Link 1', path: './templates/footer.hbs'},
+      {name: 'Link 2', path: './templates/footer.hbs'},
+      {name: 'Link 3', path: './templates/footer.hbs'}
+    ]
+  }
+  div.innerHTML = links(data)
+  document.body.appendChild(div)
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+  var div = document.createElement('div')
+  div.innerHTML = footerTemplate()
   document.body.appendChild(div)
 })
